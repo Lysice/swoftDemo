4e90be9b99b4089dc249858d1ddd85e24f1038d0
zhaoqiang <3042926672@qq.com>
2019-06-18 09:14:23 +0800

    init

---
+++ app/Application.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+
+namespace App;
+
+
+use Swoft\SwoftApplication;
+
+/**
+ * Class Application
+ *
+ * @since 2.0
+ */
+class Application extends SwoftApplication
+{
+    public function getCLoggerConfig(): array
+    {
+        $config = parent::getCLoggerConfig();
+        $config['enable'] = false;
+        return $config;
+    }
+}
